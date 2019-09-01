64cb92a662c1d003ba155c8c424505e08f8fc75b
Ronnie R. Winter <ro2nie@gmail.com>
2019-09-01 21:53:47 +0100

    Adds ronnie for the only home dir to backup at the moment

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-HOMES="ronnie"
+HOMES="$USER" #i.e: bob john joe
 DRIVE="/mnt/nas-homes"
 COMPUTER="lenovo-linux/"
 
@@ -6,17 +6,17 @@ for HOME in $HOMES; do
      FILE=/home/$HOME/rsync/.rsync-backup.lock
 
      if [ -f "$FILE" ]; then
-          echo "Another rsync process is already running on directory $HOME"
+          echo "Another rsync process is already running on directory ~/$HOME"
      else
           DESTINATION=$DRIVE/$HOME/rsync/$COMPUTER
           if [ -d "$DESTINATION" ]; then
                touch $FILE
-               echo "Initiating daily rsync backup to NAS for /home/$HOME directory"
+               echo "Initiating daily rsync backup to NAS for ~/$HOME directory"
                cd /home/$HOME
                rsync -cdlptov --exclude '.rsyncignore' --delete --log-file=log/rsync-$(date +%Y-%m-%d).log . $DESTINATION
                find . -maxdepth 1 -type d -not -name "." -exec rsync -crlptov --delete --log-file=log/rsync-$(date +%Y-%m-%d)-find.log {} $DESTINATION \;
                rm -f $FILE
-               echo "Finished daily rsync backup to NAS for /home/$HOME directory"
+               echo "Finished daily rsync backup to NAS for ~/$HOME directory"
           else
                echo "Cannot access directory $DESTINATION. Check mount state."
           fi
