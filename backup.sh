78600c181476046d254f3b02fadbbb80c4043b2d
Ronnie R. Winter <ro2nie@gmail.com>
2019-09-01 21:42:33 +0100

    Adds simple rsync daily backup script to NAS

---
+++ backup.sh
@@ -0,0 +1,25 @@
+HOMES="ronnie"
+DRIVE="/mnt/nas-homes"
+COMPUTER="lenovo-linux/"
+
+for HOME in $HOMES; do
+     FILE=/home/$HOME/rsync/.rsync-backup.lock
+
+     if [ -f "$FILE" ]; then
+          echo "Another rsync process is already running on directory $HOME"
+     else
+          DESTINATION=$DRIVE/$HOME/rsync/$COMPUTER
+          if [ -d "$DESTINATION" ]; then
+               touch $FILE
+               echo "Initiating daily rsync backup to NAS for /home/$HOME directory"
+               cd /home/$HOME
+               rsync -cdlptov --exclude '.rsyncignore' --delete --log-file=log/rsync-$(date +%Y-%m-%d).log . $DESTINATION
+               find . -maxdepth 1 -type d -not -name "." -exec rsync -crlptov --delete --log-file=log/rsync-$(date +%Y-%m-%d)-find.log {} $DESTINATION \;
+               rm -f $FILE
+               echo "Finished daily rsync backup to NAS for /home/$HOME directory"
+          else
+               echo "Cannot access directory $DESTINATION. Check mount state."
+          fi
+     fi
+done
+
